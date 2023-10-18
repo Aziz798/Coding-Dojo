5f806e7185d229a05b29232d71ec28db93cf7e99
Aziz798 <bouazizimohamedaziz76@gmail.com>
2023-10-18 13:17:55 +0100

    commit

---
+++ Programming Basics/Week 1/Day 1/Core/Algorithm_app_lvl1.js
@@ -0,0 +1,22 @@
+//----Challenge1----
+var x=5;
+console.log(x); //5
+//----Challenge2----
+var x=3;
+x=x+1;
+console.log(x); //4
+//----Challenge3----
+var x=3;
+x=x+2;
+console.log(x+2);
+console.log(x);  //7,5
+//----Challenge4----
+var x=3;
+x=x*x;
+console.log(x);
+console.log(x*2);  //9,18
+//----Challenge5----
+var x=2;
+var y=5;
+var z=x+y;
+console.log(z);  //7
