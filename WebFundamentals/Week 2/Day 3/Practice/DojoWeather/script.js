1ccaa0553ee71532539e41040631f91fde0e39f6
Aziz798 <bouazizimohamedaziz76@gmail.com>
2023-11-02 21:45:25 +0100

    week2 day2 core

---
+++ WebFundamentals/Week 2/Day 3/Practice/DojoWeather/script.js
@@ -0,0 +1,12 @@
+function sup() {
+    var element = document.querySelector('.cookies');
+    element.remove();
+}
+function change(degree) {
+    console.log(degree.value);
+    // for(i=1;i<=8;i++){
+    //     var temp=document.querySelector('#w'+i).innerHTML;
+    //     console.log(temp);
+    // }
+    var temp=document.querySelectorAll('spa')
+}
