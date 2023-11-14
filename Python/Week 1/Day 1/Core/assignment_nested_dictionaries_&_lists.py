70a117c45049c4c0da1aac69da718d749b75eca6
Aziz798 <bouazizimohamedaziz76@gmail.com>
2023-11-14 10:35:34 +0100

    user

--- Python/Week 1/Day 1/Core/assignment_nested_dictionaries_&_lists.py
+++ Python/Week 1/Day 1/Core/assignment_nested_dictionaries_&_lists.py
@@ -52,15 +52,6 @@ def iterateDictionary2(key_name, some_list):
 
 
 
-
-
-
-
-
-
-
-
-
 dojo = {
     'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
     'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
@@ -73,7 +64,7 @@ def printInfo(some_dict):
         print(f"{len(value)} {key.upper()}")
         for item in value:
             print(item)
-
+printInfo(dojo)
 
 
 
