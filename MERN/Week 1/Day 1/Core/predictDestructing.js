788e88ad5f1454c48f4c0a8c38d924cac0f1f1dd
Aziz798 <bouazizimohamedaziz76@gmail.com>
2023-12-17 12:45:11 +0100

    MERN

--- MERN/Week 1/Day 1/Core/predictDestructing.js
+++ MERN/Week 1/Day 1/Core/predictDestructing.js
@@ -46,6 +46,25 @@ console.log(first == second);
 console.log(first == third);
 //true
 
+//Problem5:
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+//value
+console.log(secondKey);
+// [1, 5, 1, 8, 3, 3]
+console.log(secondKey[0]);
+//1
+console.log(willThisWork);
+//5
+
+
 
 
 
