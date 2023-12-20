48e8ccb4b68a10d9a32532955b12d644fbef825c
Aziz798 <bouazizimohamedaziz76@gmail.com>
2023-12-20 17:04:19 +0100

    MERN

---
+++ MERN/Week 1/Day 3/Practice/prop_it_up/src/components/SaleItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SaleItem = (props) => {
+    const {prop1}=props;
+    return (
+    <div>
+        <h1>{prop1.lastName} {prop1.firstName}</h1>
+        <h4>Age : {prop1.age}</h4>
+        <h4>Hair color : {prop1.hairColor}</h4>
+    </div>
+);
+}
+
+export default SaleItem
