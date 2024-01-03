231fed66539af16354bd088251aa101db09b6ade
Aziz798 <bouazizimohamedaziz76@gmail.com>
2024-01-03 13:32:00 +0100

    MERN

--- MERN/Week 2/Day 5/Core/product manager/client/src/App.js
+++ MERN/Week 2/Day 5/Core/product manager/client/src/App.js
@@ -1,11 +1,16 @@
-import {Routes,Route} from 'react-router-dom';
+import {Routes,Route,Navigate} from 'react-router-dom';
 import './App.css';
-
+import Display from './components/Display';
+import ShowOne from './components/ShowOne';
+import Edit from './components/Edit';
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/products' element/>
+        <Route path='/' element={<Navigate to='/products'/>}/>
+        <Route path='/products' element={<Display/>}/>
+        <Route path='/products/:id' element={<ShowOne/>}/>
+        <Route path='/products/:id/edit' element={<Edit/>}/>
       </Routes>
     </div>
   );
