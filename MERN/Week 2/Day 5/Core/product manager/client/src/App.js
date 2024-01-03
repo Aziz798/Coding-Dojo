96db96128b1943c06ad0ecf01d94bce2acb085a3
Aziz798 <bouazizimohamedaziz76@gmail.com>
2024-01-03 12:28:01 +0100

    MERN

---
+++ MERN/Week 2/Day 5/Core/product manager/client/src/App.js
@@ -0,0 +1,14 @@
+import {Routes,Route} from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/products' element/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
