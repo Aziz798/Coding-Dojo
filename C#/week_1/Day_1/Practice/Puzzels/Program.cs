2743a25f5e3c43d8c1e2473983960bb891318f88
Aziz798 <bouazizimohamedaziz76@gmail.com>
2024-01-24 18:13:24 +0100

    Hello.Net

---
+++ C#/week_1/Day_1/Practice/Puzzels/Program.cs
@@ -0,0 +1,30 @@
+﻿Random rand = new();
+string CoinFlip()
+{
+
+    int value = rand.Next(0, 2);
+    if (value == 0)
+    {
+        return "Heads";
+    }
+    return "Tails";
+
+}
+Console.WriteLine(CoinFlip());
+
+int DiceRoll()
+{
+    int[] dice = [1, 2, 3, 4, 5, 6];
+    return dice[rand.Next(dice.Length)];
+}
+Console.WriteLine(DiceRoll());
+
+List<int> RollDiceMultipleTimes(int time)
+{
+    List<int> RolledDices = new List<int>();
+    for (int i = 0; i <= time; i++)
+    {
+        RolledDices.Add(DiceRoll());
+    }
+    return RolledDices;
+}
